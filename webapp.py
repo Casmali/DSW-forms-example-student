b903a4bb4c1a2d89d7bc26adda392b40c0977970
f.c.lopez <casmali.f.lopez@gmail.com>
2017-10-09 09:32:24 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,6 +8,15 @@ def render_main():
 
 @app.route("/response")
 def render_response():
+        color = request.args['color']
+        #The request object stores information that was sent by the client to the server.
+        #the args is a multidict
+        #the way we get info from args is that it is visible in a url. - the information in args is visible in the url for hte page being requested(ex. .../response?color=blue)
+        if color is 'black':
+            response = 'Thats my favorite color too!'
+        else:
+            response = 'ewww'
+        renturn render_template('response.html', response = responce)
     
     
 if __name__=="__main__":
